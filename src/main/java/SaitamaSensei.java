7967781dcd5a5649a754a8ef7644ffba097a5272
Javier <136307603+javnz1@users.noreply.github.com>
2026-01-20 02:15:26 +0800

    Level-1: Implement echo commands and exit with bye command

--- src/main/java/SaitamaSensei.java
+++ src/main/java/SaitamaSensei.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class SaitamaSensei {
     private static final String HORIZONTAL_LINE = "____________________________________________________________";
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(HORIZONTAL_LINE);
         System.out.println("Hello! I'm Saitama Sensei!");
         System.out.println("What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+
+        String command = scanner.nextLine();
+
+        while (!command.equalsIgnoreCase("bye")) {
+            System.out.println(HORIZONTAL_LINE);
+            System.out.println(command);
+            System.out.println(HORIZONTAL_LINE);
+
+            command = scanner.nextLine();
+        }
         System.out.println(HORIZONTAL_LINE);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(HORIZONTAL_LINE);
