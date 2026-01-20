2a9e5da204c06439bdf7bfd79eceb9d800f8ccaf
Javier <136307603+javnz1@users.noreply.github.com>
2026-01-20 19:04:29 +0800

    Level-4: Implement tracking for ToDos, Events, Deadlines tasks

--- src/main/java/SaitamaSensei.java
+++ src/main/java/SaitamaSensei.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 
 public class SaitamaSensei {
     private static final String HORIZONTAL_LINE = "____________________________________________________________";
+    private static ArrayList<Task> taskList;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Task> taskList = new ArrayList<>();
+        taskList = new ArrayList<>();
 
         System.out.println(HORIZONTAL_LINE);
         System.out.println("Hello! I'm Saitama Sensei!");
@@ -37,18 +38,45 @@ public class SaitamaSensei {
                 taskList.get(num).unmarkAsDone();
                 System.out.println(taskList.get(num));
                 System.out.println(HORIZONTAL_LINE);
-            } else {
-                taskList.add(new Task(command));
+            } else if (command.startsWith("todo")) {
+                String description = command.replace("todo ", "").trim();
 
-                System.out.println(HORIZONTAL_LINE);
-                System.out.println("Added: " + command);
-                System.out.println(HORIZONTAL_LINE);
+                Task new_task = new ToDos(description);
+                taskList.add(new_task);
+                taskString(new_task);
+            } else if (command.startsWith("deadline")) {
+                String[] subcommand = command.split("/by ");
+                String description = subcommand[0].replace("deadline ", "").trim();
+                String by = subcommand[1];
+
+                Task new_task = new Deadline(description,by);
+                taskList.add(new_task);
+                taskString(new_task);
+            } else if (command.startsWith("event")) {
+                String[] subcommand = command.split("/from ");
+                String description = subcommand[0].replace("event ", "").trim();
+
+                String[] date = subcommand[1].split("/to ");
+                String from = date[0].trim();
+                String to = date[1].trim();
+
+                Task new_task = new Events(description,from, to);
+                taskList.add(new_task);
+                taskString(new_task);
             }
 
             command = scanner.nextLine();
         }
         System.out.println(HORIZONTAL_LINE);
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void taskString(Task task){
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(task);
+        System.out.println("Now you have " + taskList.size() + " tasks in the list.");
         System.out.println(HORIZONTAL_LINE);
     }
 }
