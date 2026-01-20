5171bdd8d257af0e9134600c232630dc69c9efae
Javier <136307603+javnz1@users.noreply.github.com>
2026-01-20 16:23:01 +0800

    Level-3: Implement ability to mark and unmark tasks as done

--- src/main/java/SaitamaSensei.java
+++ src/main/java/SaitamaSensei.java
@@ -6,7 +6,7 @@ public class SaitamaSensei {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> taskList = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
 
         System.out.println(HORIZONTAL_LINE);
         System.out.println("Hello! I'm Saitama Sensei!");
@@ -16,17 +16,32 @@ public class SaitamaSensei {
         String command = scanner.nextLine();
 
         while (!command.equalsIgnoreCase("bye")) {
-            if (!command.equalsIgnoreCase("list")){
-                taskList.add(command);
+            if (command.equalsIgnoreCase("list")){
+                System.out.println(HORIZONTAL_LINE);
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < taskList.size(); i++){
+                    System.out.println((i+1) + "." + taskList.get(i));
+                }
+                System.out.println(HORIZONTAL_LINE);
+            } else if (command.startsWith("mark")){
+                int num = Integer.parseInt(command.split(" ")[1]) - 1;
+                System.out.println(HORIZONTAL_LINE);
+                System.out.println("Nice! I've marked this task as done:");
+                taskList.get(num).markAsDone();
+                System.out.println(taskList.get(num));
+                System.out.println(HORIZONTAL_LINE);
+            } else if (command.startsWith("unmark")){
+                int num = Integer.parseInt(command.split(" ")[1]) - 1;
+                System.out.println(HORIZONTAL_LINE);
+                System.out.println("OK, I've marked this task as not done yet:");
+                taskList.get(num).unmarkAsDone();
+                System.out.println(taskList.get(num));
+                System.out.println(HORIZONTAL_LINE);
+            } else {
+                taskList.add(new Task(command));
 
                 System.out.println(HORIZONTAL_LINE);
                 System.out.println("Added: " + command);
-                System.out.println(HORIZONTAL_LINE);
-            } else {
-                System.out.println(HORIZONTAL_LINE);
-                for (int i = 0; i < taskList.size(); i++){
-                    System.out.println((i+1) + ". " + taskList.get(i));
-                }
                 System.out.println(HORIZONTAL_LINE);
             }
 
